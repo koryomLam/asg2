7ed49e8b136c93ceb75165a93310794023843fb6
Koryom Lam <929779@ACDM.DS.SAIT.CA>
2024-01-26 23:45:17 -0700

    Initial commit with Nuxt setup and API

---
+++ Disney.js
@@ -0,0 +1,8 @@
+// server/api/disney.js
+export const disneyCharacters = [
+    { id: 1, name: 'Mickey Mouse' },
+    { id: 2, name: 'Donald Duck' },
+    { id: 3, name: 'Goofy' },
+    // Add more characters as needed
+  ];
+  
